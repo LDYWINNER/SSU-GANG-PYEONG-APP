a7a392a4d3896da67372997ef3d9f04f0165ba0d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-10-14 18:20:44 -0400

    ADD - types

--- ssu-gang-pyeong/App.tsx
+++ ssu-gang-pyeong/App.tsx
@@ -1,11 +1,24 @@
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import React, { useState } from "react";
+import { Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 type Props = {
-  Ionicons: keyof typeof Ionicons.glyphMap;
+  Ionicons: keyof typeof Ionicons.glyphMap | any;
 };
+
+const loadFonts = (fonts: any[]) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images: any[]) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App({ Ionicons }: Props) {
   const [ready, setReady] = useState(false);
@@ -13,7 +26,9 @@ export default function App({ Ionicons }: Props) {
     setReady(true);
   };
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons);
+    const images = loadImages([require("./"), require("./")]);
+    const fonts = loadFonts([Ionicons.font]);
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
