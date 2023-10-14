e2ebf656cb40069e84b4126ec89539c9d5a09eaf
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-10-14 16:27:09 -0400

    ADD - app loading

--- ssu-gang-pyeong/App.tsx
+++ ssu-gang-pyeong/App.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function App() {
+type Props = {
+  Ionicons: keyof typeof Ionicons.glyphMap;
+};
+
+export default function App({ Ionicons }: Props) {
+  const [ready, setReady] = useState(false);
+  const onFinish = () => {
+    setReady(true);
+  };
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons);
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
   return null;
 }
