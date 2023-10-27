6124bb08abd0a38d13c76fbeabd85d22197df183
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-10-26 23:11:17 -0400

    UPDATE - Stack nav screen option

--- ssu-gang-pyeong/navigation/Stack.tsx
+++ ssu-gang-pyeong/navigation/Stack.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { CourseDetail } from "../screens";
+import { useColorScheme } from "react-native";
+import colors from "../colors";
 
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => {
+  const isDark = useColorScheme() === "dark";
   return (
-    <NativeStack.Navigator>
+    <NativeStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: isDark ? colors.BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : colors.BLACK_COLOR,
+        },
+      }}
+    >
       <NativeStack.Screen name="CourseDetail" component={CourseDetail} />
     </NativeStack.Navigator>
   );
