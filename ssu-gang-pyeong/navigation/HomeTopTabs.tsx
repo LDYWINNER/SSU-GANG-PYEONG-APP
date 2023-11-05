2ad162fee719196c2f1c0ffcc2836e0df5bf4f97
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-11-05 01:41:47 -0400

    UPDATE - home tableview screen top label

--- ssu-gang-pyeong/navigation/HomeTopTabs.tsx
+++ ssu-gang-pyeong/navigation/HomeTopTabs.tsx
@@ -1,17 +1,87 @@
+import React, { useState } from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { CourseDetail } from "../screens";
+import styled from "styled-components/native";
+import { TouchableOpacity, View } from "react-native";
+import { useColorScheme } from "react-native";
+import colors from "../colors";
+import { Ionicons } from "@expo/vector-icons";
+
+const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const BigRow = styled(Row)`
+  justify-content: space-between;
+  margin-top: 70px;
+`;
+
+const Title = styled.Text`
+  color: black;
+  font-weight: 600;
+  font-size: 30px;
+  margin-left: 10px;
+`;
 
 const semesters = ["2023-fall", "2023-spring", "2022-fall"];
 
 const Tab = createMaterialTopTabNavigator();
 
 function HomeTopTabs() {
+  const isDark = useColorScheme() === "dark";
+  const color = isDark ? "white" : colors.BLACK_COLOR;
+  const [tableView, setTableView] = useState(true);
+  const toggleView = () => setTableView((current) => !current);
   return (
-    <Tab.Navigator>
-      {semesters.map((semester) => (
-        <Tab.Screen key={semester} name={semester} component={CourseDetail} />
-      ))}
-    </Tab.Navigator>
+    <>
+      <BigRow>
+        <Title>@USERNAME</Title>
+        <Row>
+          <TouchableOpacity>
+            <Ionicons
+              name={isDark ? "add-circle-outline" : "add-circle"}
+              color={color}
+              size={35}
+              style={{ marginRight: 1 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons
+              name={
+                isDark
+                  ? "ellipsis-horizontal-circle-outline"
+                  : "ellipsis-horizontal-circle"
+              }
+              color={color}
+              size={35}
+              style={{ marginRight: 1 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toggleView()}>
+            <Ionicons
+              name={tableView ? "grid" : "grid-outline"}
+              color={color}
+              size={30}
+              style={{ marginRight: 1 }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toggleView()}>
+            <Ionicons
+              name={!tableView ? "menu" : "menu-outline"}
+              color={color}
+              size={45}
+              style={{ marginRight: 1 }}
+            />
+          </TouchableOpacity>
+        </Row>
+      </BigRow>
+      <Tab.Navigator>
+        {semesters.map((semester) => (
+          <Tab.Screen key={semester} name={semester} component={CourseDetail} />
+        ))}
+      </Tab.Navigator>
+    </>
   );
 }
 
--- ssu-gang-pyeong/navigation/mainStacks/HomeStack.tsx
+++ ssu-gang-pyeong/navigation/mainStacks/HomeStack.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useColorScheme } from "react-native";
-import colors from "../../colors";
 import HomeTopTabs from "../HomeTopTabs";
 import { ListView } from "../../screens/homeScreens/homeIndex";
 
 const NativeStack = createNativeStackNavigator();
 
 const HomeStack = () => {
-  const isDark = useColorScheme() === "dark";
   return (
     <NativeStack.Navigator
       screenOptions={{
-        headerBackTitleVisible: false,
-        headerStyle: {
-          backgroundColor: isDark ? colors.BLACK_COLOR : "white",
-        },
-        headerTitleStyle: {
-          color: isDark ? "white" : colors.BLACK_COLOR,
-        },
+        headerShown: false,
       }}
     >
       <NativeStack.Screen name="HomeTopTabs" component={HomeTopTabs} />
