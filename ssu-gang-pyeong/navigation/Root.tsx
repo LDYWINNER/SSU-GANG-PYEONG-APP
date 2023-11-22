9356b9bb5a07d1e1249c5bd6f84dc47ff3aa8e48
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-11-22 14:53:16 -0500

    SETUP - Zustand + AuthStack and MainStack

--- ssu-gang-pyeong/navigation/Root.tsx
+++ ssu-gang-pyeong/navigation/Root.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainTabs from "./MainTabs";
 import MainStack from "./MainStack";
+import useUserGlobalStore from "../store/useUserGlobal";
+import AuthStack from "./AuthStack";
 
 const Nav = createNativeStackNavigator();
 
-const Root = () => (
-  <Nav.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-  >
-    <Nav.Screen name="MainTabs" component={MainTabs} />
-    <Nav.Screen name="MainStack" component={MainStack} />
-  </Nav.Navigator>
-);
+const Root = () => {
+  const { user, updateUser } = useUserGlobalStore();
+
+  useEffect(() => {
+    updateUser({
+      username: "",
+      email: "",
+      school: "",
+      major: "",
+      courseReviewNum: 0,
+      adminAccount: false,
+    });
+  }, []);
+
+  return (
+    <>
+      {user ? (
+        <Nav.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Nav.Screen name="MainTabs" component={MainTabs} />
+          <Nav.Screen name="MainStack" component={MainStack} />
+        </Nav.Navigator>
+      ) : (
+        <AuthStack />
+      )}
+    </>
+  );
+};
 export default Root;
