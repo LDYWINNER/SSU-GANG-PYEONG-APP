c5d803887dc77003bcb218c35195e6129d9fcac3
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-12-09 01:19:25 -0500

    FIX - GET tableView Courses - easy pick

--- ssu-gang-pyeong/screens/homeScreens/EasyPick.tsx
+++ ssu-gang-pyeong/screens/homeScreens/EasyPick.tsx
@@ -1,46 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import useSWR from "swr";
+import { Loader } from "../../components";
 import useGlobalToggle from "../../store/useGlobalToggle";
-import useUserGlobalStore from "../../store/useUserGlobal";
 import { Box, Text } from "../../theme";
+import { ICourse } from "../../types";
 import { fetcher } from "../../utils/config";
 
-const getCourseRequest = async (url: string, currentCourse: string) => {
-  try {
-    return await fetcher(url + "/" + currentCourse);
-  } catch (error) {
-    console.log("error in getCourseRequest", error);
-    throw error;
-  }
-};
-
 const EasyPick = () => {
-  const { user } = useUserGlobalStore();
   const { toggleInfo } = useGlobalToggle();
 
-  const { data: course, mutate } = useSWR(
-    user &&
-      toggleInfo &&
-      `/api/v1/course/${user.classHistory[toggleInfo.currentTableView]}`,
-    fetcher
-  );
+  const { data: courses, isLoading: isLoadingCourses } = useSWR<{
+    takingCourses: ICourse[];
+  }>(`/api/v1/course/tableView/${toggleInfo?.currentTableView}`, fetcher);
 
-  useEffect(() => {
-    user!.classHistory[toggleInfo!.currentTableView].forEach(
-      (currentCourse) => {
-        getCourseRequest("/api/v1/course", currentCourse).then((data) =>
-          mutate(data, false)
-        ); // Mutate SWR cache
-      }
-    );
-  }, []);
-
+  if (isLoadingCourses) {
+    return <Loader />;
+  }
   return (
     <Box flex={1}>
       <ScrollView>
         <Text>{toggleInfo?.currentTableView}</Text>
-        <Text>{course && <Text>{JSON.stringify(course._id)}</Text>}</Text>
+        {courses!.takingCourses.map((courseItem) => (
+          <Text key={courseItem._id}>{courseItem.crs}</Text>
+        ))}
       </ScrollView>
     </Box>
   );
