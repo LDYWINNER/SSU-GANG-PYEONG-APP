cb09c79e15874b0a2c7dd1ed7c82a6cf553add9d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-02-18 03:15:33 -0500

    ADD - MyAccount layout

--- ssu-gang-pyeong/screens/userScreens/MyAccount.tsx
+++ ssu-gang-pyeong/screens/userScreens/MyAccount.tsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Box, Text, Theme } from "../../theme";
+import { NavigateBack, SafeAreaWrapper } from "../../components";
 
 const MyAccount = () => (
-  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    <Text>MyAccount</Text>
-  </View>
+  <SafeAreaWrapper>
+    <Box flex={1} mx="4">
+      <Box
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <NavigateBack />
+        <Text
+          variant="text2Xl"
+          fontWeight="700"
+          textDecorationLine="underline"
+          textDecorationColor="iconBlue"
+          textDecorationStyle="double"
+        >
+          My Account
+        </Text>
+        <Box mr="10"></Box>
+      </Box>
+      <Box height={16} />
+
+      <Box></Box>
+    </Box>
+  </SafeAreaWrapper>
 );
 
 export default MyAccount;
